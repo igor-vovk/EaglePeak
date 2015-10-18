fe88e1ec38a3d77a2aad5e95cd575b2c3d63d1a3
Igor Vovk <adwolf15@gmail.com>
2015-10-18 13:47:23 +0300

    tracing serialization process

--- src/main/scala/com/igorvovk/eaglepeak/io/Serialization.scala
+++ src/main/scala/com/igorvovk/eaglepeak/io/Serialization.scala
@@ -4,15 +4,18 @@ import java.io.File
 
 import breeze.io.FileStreams
 import com.google.inject.{Inject, Singleton}
+import com.igorvovk.eaglepeak.service.Logging
 import com.twitter.chill.KryoPool
 import org.apache.commons.io.IOUtils
 
 import scala.reflect.ClassTag
 
 @Singleton
-class Serialization @Inject()(pool: KryoPool) {
+class Serialization @Inject()(pool: KryoPool) extends Logging {
 
   def save[T: ClassTag](obj: T, path: String) = {
+    trace(s"Serialize $path")
+
     val file = new File(path)
     if (file.exists()) {
       file.delete()
@@ -35,6 +38,8 @@ class Serialization @Inject()(pool: KryoPool) {
   }
 
   def load[T: ClassTag](path: String): T = {
+    trace(s"Deserialize $path")
+
     val is = FileStreams.input(new File(path))
     val kryo = pool.borrow()
 
